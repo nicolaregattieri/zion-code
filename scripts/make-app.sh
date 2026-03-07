acf4a338aa8a688500ee177088a3e0fea2905468
Nicola <nicregattieri@gmail.com>
2026-03-07 02:10:34 -0300

    chore(release): bump version to 1.6.3 (build 24)

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -54,9 +54,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>23</string>
+  <string>24</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.2</string>
+  <string>1.6.3</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
