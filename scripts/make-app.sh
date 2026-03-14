3852cbb87b08027af658c882e834246eebca877e
Nicola <nicregattieri@gmail.com>
2026-03-14 03:12:24 -0300

    release: bump version to 1.6.10

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>30</string>
+  <string>31</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.9</string>
+  <string>1.6.10</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
