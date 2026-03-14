f0fde1375e47ccce2db384d86c74d596d23b37d3
Nicola <nicregattieri@gmail.com>
2026-03-14 12:02:44 -0300

    chore(release): Update app version to 1.6.14
    - Increment CFBundleVersion to 35.
    - Update CFBundleShortVersionString to 1.6.14 in Info.plist.

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>34</string>
+  <string>35</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.13</string>
+  <string>1.6.14</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
