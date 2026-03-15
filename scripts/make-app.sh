f92c516a21dc7e91561de4d74eed1c65d75fe00b
Nicola <nicregattieri@gmail.com>
2026-03-14 23:03:49 -0300

    build(scripts): Update app version and build number
    - Increment CFBundleVersion to 38
    - Update CFBundleShortVersionString to 1.6.17

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>37</string>
+  <string>38</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.16</string>
+  <string>1.6.17</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
