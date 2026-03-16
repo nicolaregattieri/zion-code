b62b9be17034283abf29e3ac28880e02479e570d
Nicola <nicregattieri@gmail.com>
2026-03-15 23:38:50 -0300

    chore: Update app version and build number
    - Increment `CFBundleVersion` to 40.
    - Update `CFBundleShortVersionString` to 1.6.19.

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>39</string>
+  <string>40</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.18</string>
+  <string>1.6.19</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
