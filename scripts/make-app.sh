b2fc77733ac6e1f685dc5dbab913b352c1af7ee0
Nicola <nicregattieri@gmail.com>
2026-03-19 17:10:43 -0300

    chore(scripts): Increment app version for release 1.6.24
    - Update CFBundleVersion to 45
    - Update CFBundleShortVersionString to 1.6.24

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>44</string>
+  <string>45</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.23</string>
+  <string>1.6.24</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
