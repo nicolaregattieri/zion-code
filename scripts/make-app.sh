6902afaf7c9007f6afee0263d54570af8f23870a
Nicola <nicregattieri@gmail.com>
2026-03-21 12:09:05 -0300

    chore: bump version to 1.7.0 (build 50)

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -61,9 +61,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>49</string>
+  <string>50</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.6.28</string>
+  <string>1.7.0</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
