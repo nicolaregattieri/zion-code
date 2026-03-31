272557c9a6ef2ea137db7fdd61cdf0a076bdab9a
Nicola <nicregattieri@gmail.com>
2026-03-30 23:20:03 -0300

    chore(scripts): Increment app version for release 1.7.16

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -86,9 +86,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>65</string>
+  <string>66</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.7.15</string>
+  <string>1.7.16</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
