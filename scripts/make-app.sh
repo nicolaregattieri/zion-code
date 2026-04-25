ce44c9c5878457249d2c85f5130ae0a7b687ce5d
Nicola Regattieri <nicolaregattieri@Nicolas-MacBook-Pro.local>
2026-04-25 01:13:22 -0300

    chore(scripts): Increment app version for release 1.7.35

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -86,9 +86,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>84</string>
+  <string>85</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.7.34</string>
+  <string>1.7.35</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
