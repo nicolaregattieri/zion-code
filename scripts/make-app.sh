4b49d959809fb570e69882e9604505925c4820e9
Your Name <you@example.com>
2026-04-27 20:28:09 -0300

    chore(scripts): Increment app version for release 1.7.36

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -86,9 +86,9 @@ cat > "$APP_DIR/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key>
   <string>com.nicolaregattieri.zion.app</string>
   <key>CFBundleVersion</key>
-  <string>85</string>
+  <string>86</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.7.35</string>
+  <string>1.7.36</string>
   <key>CFBundleExecutable</key>
   <string>Zion</string>
   <key>CFBundlePackageType</key>
